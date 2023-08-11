14a55a4cf46708bb241c5187d27e610305716784
Jaewoong <dixk3458@naver.com>
2023-08-11 15:56:30 +0900

    Observer-Part(1)

---
+++ src/active_menu.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const sectionIds = [
+  '#home',
+  '#about',
+  '#skills',
+  '#work',
+  '#testimonial',
+  '#contact',
+];
+
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => document.querySelector(`[href="${id}"]`));
+const visibleSections = sectionIds.map(() => false);
+const options = {};
+const observer = new IntersectionObserver(observerCallback, options);
+
+sections.forEach(section => observer.observe(section));
+
+function observerCallback(entries) {
+  let selectLastOne;
+  entries.forEach(entry => {
+    const index = sectionIds.indexOf(`#${entry.target.id}`);
+    visibleSections[index] = entry.isIntersecting;
+    console.log('index', index);
+    selectLastOne =
+      index === sectionIds.length - 1 &&
+      entry.isIntersecting &&
+      entry.intersectingRatio >= 0.99;
+  });
+
+  console.log('마지막:', selectLastOne);
+  const navIndex = selectLastOne
+    ? sectionIds.length - 1
+    : findFirstIntersecting(visibleSections);
+
+  console.log(sectionIds[navIndex]);
+}
+
+function findFirstIntersecting(intersections) {
+  const index = intersections.indexOf(true);
+  return index >= 0 ? index : 0;
+}
