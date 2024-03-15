4a6bf4d4b2e2297b4c3123ffe13ef113a4653758
Jaewoong <dixk3458@naver.com>
2024-03-15 15:43:52 +0900

    update

--- src/type.js
+++ src/type.js
@@ -22,7 +22,12 @@ function handleHomeDescriptionInstance(instance) {
     .move(-7) //
     .move(null, { to: 'END' })
     .delete() //
-    .type('흡수력이 빠른')
+    .type('하고싶은')
+    .pause(1000)
+    .move(-6)
+    .move(null, { to: 'END' })
+    .delete()
+    .type('성장해 나가고 싶은')
     .pause(1000)
     .move(-6)
     .move(null, { to: 'END' })
